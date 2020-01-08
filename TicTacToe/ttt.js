4b5e0dbbb1c68270c15d1bed237680f3a42511ba
Aireec Joseph <37174536+Aireec@users.noreply.github.com>
2020-01-08 15:54:26 +0200

    Add files via upload

---
+++ TicTacToe/ttt.js
@@ -0,0 +1,122 @@
+var makeOne = document.querySelector('#one')
+var makeTwo = document.querySelector('#two')
+var makeThree = document.querySelector('#three')
+var makeFour = document.querySelector('#four')
+var makeFive = document.querySelector('#five')
+var makeSix = document.querySelector('#six')
+var makeSeven = document.querySelector('#seven')
+var makeEight = document.querySelector('#eight')
+var makeNine = document.querySelector('#nine')
+
+makeOne.addEventListener('click', function(){
+  makeOne.textContent = 'X';
+  makeOne.style.color = 'red';
+  console.log("CLCIKED");
+})
+
+makeOne.addEventListener('dblclick', function(){
+  makeOne.textContent = 'O';
+  makeOne.style.color = 'blue';
+  console.log("CLCIKED");
+})
+
+makeTwo.addEventListener('click', function(){
+  makeTwo.textContent = 'X';
+  makeTwo.style.color = 'red';
+  console.log("CLCIKED");
+})
+
+makeTwo.addEventListener('dblclick', function(){
+  makeTwo.textContent = 'O';
+  makeTwo.style.color = 'blue';
+  console.log("CLCIKED");
+})
+
+makeThree.addEventListener('click', function(){
+  makeThree.textContent = 'X';
+  makeThree.style.color = 'red';
+  console.log("CLCIKED");
+})
+
+makeThree.addEventListener('dblclick', function(){
+  makeThree.textContent = 'O';
+  makeThree.style.color = 'blue';
+  console.log("CLCIKED");
+})
+
+makeFour.addEventListener('click', function(){
+  makeFour.textContent = 'X';
+  makeFour.style.color = 'red';
+  console.log("CLCIKED");
+})
+
+makeFour.addEventListener('dblclick', function(){
+  makeFour.textContent = 'O';
+  makeFour.style.color = 'blue';
+  console.log("CLCIKED");
+})
+
+makeFive.addEventListener('click', function(){
+  makeFive.textContent = 'X';
+  makeFive.style.color = 'red';
+  console.log("CLCIKED");
+})
+
+makeFive.addEventListener('dblclick', function(){
+  makeFive.textContent = 'O';
+  makeFive.style.color = 'blue';
+  console.log("CLCIKED");
+})
+
+makeSix.addEventListener('click', function(){
+  makeSix.textContent = 'X';
+  makeSix.style.color = 'red';
+  console.log("CLCIKED");
+})
+
+makeSix.addEventListener('dblclick', function(){
+  makeSix.textContent = 'O';
+  makeSix.style.color = 'blue';
+  console.log("CLCIKED");
+})
+
+makeSeven.addEventListener('click', function(){
+  makeSeven.textContent = 'X';
+  makeSeven.style.color = 'red';
+  console.log("CLCIKED");
+})
+
+makeSeven.addEventListener('dblclick', function(){
+  makeSeven.textContent = 'O';
+  makeSeven.style.color = 'blue';
+  console.log("CLCIKED");
+})
+
+makeEight.addEventListener('click', function(){
+  makeEight.textContent = 'X';
+  makeEight.style.color = 'red';
+  console.log("CLCIKED");
+})
+
+makeEight.addEventListener('dblclick', function(){
+  makeEight.textContent = 'O';
+  makeEight.style.color = 'blue';
+  console.log("CLCIKED");
+})
+
+makeNine.addEventListener('click', function(){
+  makeNine.textContent = 'X';
+  makeNine.style.color = 'red';
+  console.log("CLCIKED");
+})
+
+makeNine.addEventListener('dblclick', function(){
+  makeNine.textContent = 'O';
+  makeNine.style.color = 'blue';
+  console.log("CLCIKED");
+})
+
+
+function refreshPage(){
+    window.location.reload();
+} 
